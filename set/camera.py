12f46573ce3e97c892aa444746c4da615c170c4f
Jeff Shen <shenjeff@princeton.edu>
2023-03-06 22:07:08 -0500

    we are getting there!

---
+++ set/camera.py
@@ -0,0 +1,43 @@
+import serial
+import time
+import numpy as np
+import cv2
+from ultralytics import YOLO
+
+# with serial.Serial("/dev/cu.usbmodem3101", 9600, timeout=1) as ser:
+#     time.sleep(2)
+#     for i in range(100):
+#         i, j = np.random.randint(1, 4, size=(2, 3)).astype(int)
+#         print(f"{i},{j}")
+#         # check that the random number pairs are not the same
+#         if (
+#             (i[0] == i[1] and j[0] == j[1])
+#             or (i[0] == i[2] and j[0] == j[2])
+#             or (i[1] == i[2] and j[1] == j[2])
+#         ):
+#             continue
+#         ser.write(bytes(f"{i[0]},{j[0]}\n", "utf-8"))
+#         ser.write(bytes(f"{i[1]},{j[1]}\n", "utf-8"))
+#         ser.write(bytes(f"{i[2]},{j[2]}\n", "utf-8"))
+#         time.sleep(0.5)
+#         ser.write(bytes(f"{i[0]},{j[0]}\n", "utf-8"))
+#         ser.write(bytes(f"{i[1]},{j[1]}\n", "utf-8"))
+#         ser.write(bytes(f"{i[2]},{j[2]}\n", "utf-8"))
+#         time.sleep(0.1)
+
+yolo = YOLO("yolov8n.pt", task="detect")
+
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret, frame = cap.read()
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    res = yolo.predict(rgb[:, :, :3])
+    res_plotted = res[0].plot()
+    cv2.imshow("result", res_plotted)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.imwrite("test.png", rgb)
+        break
+
+cap.release()
+cv2.destroyAllWindows()
